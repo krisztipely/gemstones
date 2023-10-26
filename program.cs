7ca6fdfad4336abf94a5c5769ec3bfbbac0c57d3
krisztina.lakner <kriszti.lakner93@gmail.com>
2023-10-26 08:11:02 +0200

    remove unnessecary comments

--- program.cs
+++ program.cs
@@ -6,17 +6,15 @@ class Program
     {
         GemstoneRepository gemstoneRepository = new GemstoneRepository();
 
-        // Example: Adding gemstones
+        //Adding gemstones
         gemstoneRepository.AddGemstone(new Gemstone { Id = 1, Name = "Diamond", Weight = 2.5, Color = "Clear" });
         gemstoneRepository.AddGemstone(new Gemstone { Id = 2, Name = "Ruby", Weight = 1.8, Color = "Red" });
         gemstoneRepository.AddGemstone(new Gemstone { Id = 3, Name = "Sapphire", Weight = 2.2, Color = "Blue" });
 
-        // Example: Displaying gemstones
+        //Displaying gemstones
         Console.WriteLine("List of Gemstones:");
         gemstoneRepository.DisplayGemstones();
 
-        // You can continue building your application with more features as needed.
 
-        Console.ReadLine(); // Keep console window open in Visual Studio
-    }
+        Console.ReadLine(); 
 }
